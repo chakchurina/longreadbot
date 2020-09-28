fb890dbcd6eae8c418cdcec4ed857df0b6670f7e
camillerakhmetov <maria.chakchurina@yandex.ru>
2020-09-28 15:24:28 +0300

    ADD: second message support

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 # run me from root folder: python run_longread_bot.py
 
 import os
+from datetime import datetime
+import time
 import logging
 from telegram import Bot, InputMediaPhoto, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, ConversationHandler, Filters, MessageHandler, CallbackQueryHandler
@@ -16,14 +18,19 @@ class LongreadBot:
 
     lang = None
     bot = None
-    SET_LANG, RENDER = range(2)
+    text = None
 
-    def __init__(self):
+    SET_LANG, GET = range(2)
+
+    tasks_to_do = None
+
+    def __init__(self, tasks_to_do):
         self.bot = Bot(token=TELEGRAM_TOKEN)
+        self.tasks_to_do = tasks_to_do
 
     def start(self, update, context):
-        keyboard = [[InlineKeyboardButton("Русский", callback_data='ru'),
-                     InlineKeyboardButton("Английский", callback_data='en')]]
+        keyboard = [[InlineKeyboardButton("Russian", callback_data='ru'),
+                     InlineKeyboardButton("English", callback_data='en')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text(text="Choose your language", reply_markup=reply_markup)
 
@@ -41,51 +48,77 @@ class LongreadBot:
         chat_id = query.message.chat_id
         self.bot.send_message(chat_id=chat_id, text=message_texts.MESSAGE_START(self.lang))
 
-        return self.RENDER
+        return self.GET
 
-    def render(self, update, context):
+    def get(self, update, context):
         text = update.message.text
-        context.user_data['choice'] = text
+        chat_id = update.message.chat["id"]
 
-        r = Renderer()
-        text, images = r.render(text)
-        update.message.reply_text(text)
+        task = {
+            "text": text,
+            "chat_id": chat_id,
+            "timestamp": datetime.now().timestamp(),
+        }
+        self.tasks_to_do.put(task)
+        return self.GET
 
-        if images:
-            chat_id = update.message.chat["id"]
-            arr = []
-            for it, image in enumerate(images):
-                arr.append(InputMediaPhoto(open(image, 'rb')))
-            self.bot.send_media_group(chat_id=chat_id, media=arr)
+    def render(self, tasks_to_do):
 
-            for image in images:
-                os.remove(image)
+        while True:
+            if tasks_to_do.empty():
+                time.sleep(0.2)
+                continue
 
-        update.message.reply_text(message_texts.RENDERED(self.lang))
+            task = tasks_to_do.get()
 
-        return self.RENDER
+            text = task["text"]
+            chat_id = task["chat_id"]
+            ts = task["timestamp"]
+
+            time.sleep(0.2)
+
+            if not tasks_to_do.empty():
+                other_task = tasks_to_do.get()
+                other_text = other_task["text"]
+                other_chat_id = other_task["chat_id"]
+                other_ts = other_task["timestamp"]
+
+                if other_chat_id == chat_id and 0 < ts - other_ts < 0.5:
+                    text = other_text + text
+                elif other_chat_id == chat_id and -0.5 < ts - other_ts < 0:
+                    text = text + other_text
+                else:
+                    tasks_to_do.put(other_task)
+
+            r = Renderer()
+            text, images = r.render(text)
+
+            self.bot.send_message(chat_id=chat_id, text=text)
+
+            if images:
+                arr = []
+                for it, image in enumerate(images):
+                    arr.append(InputMediaPhoto(open(image, 'rb')))
+                self.bot.send_media_group(chat_id=chat_id, media=arr)
+
+                for image in images:
+                    os.remove(image)
+
+            self.bot.send_message(chat_id=chat_id, text=message_texts.RENDERED(self.lang))
 
     def done(self, update, context):
-        user_data = context.user_data
-        if 'choice' in user_data:
-            del user_data['choice']
-
-        update.message.reply_text("Bye!")
-
-        user_data.clear()
-        return ConversationHandler.END
+        return self.GET
 
     def main(self):
-        updater = Updater(TELEGRAM_TOKEN, use_context=True)
+        updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
         dp = updater.dispatcher
 
         conv_handler = ConversationHandler(
             entry_points=[CommandHandler('start', self.start)],
             states={
-                self.RENDER: [MessageHandler(Filters.regex('.*'), self.render, pass_user_data=True)],
+                self.GET: [MessageHandler(Filters.regex('.*'), self.get, pass_user_data=True)],
                 self.SET_LANG: [CallbackQueryHandler(self.set_lang, pass_user_data=True, pattern=r'.*')]
             },
-
             fallbacks=[MessageHandler(Filters.regex('^Done$'), self.done)]
         )
 
--- run_bot.py
+++ run_bot.py
@@ -1,4 +1,17 @@
+from multiprocessing import Lock, Process, Queue, current_process
 from bot import LongreadBot
 
+
 if __name__ == "__main__":
-    LongreadBot().main()
+
+    tasks_to_do = Queue()
+
+    bot = LongreadBot(tasks_to_do)
+    render_p = Process(target=LongreadBot.render, args=(bot, tasks_to_do))
+    render_p.start()
+
+    bot_p = Process(target=LongreadBot.main, args=(bot,))
+    bot_p.start()
+
+    render_p.join()
+    bot_p.join()
